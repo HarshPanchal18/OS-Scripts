1985238665e736f8858583c6e83ffc2d631b994f
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-04-20 12:20:43 +0530

    Update readVariable.sh

--- readVariable.sh
+++ readVariable.sh
@@ -4,3 +4,15 @@ echo "What's your name"
 read name
 
 echo "I'm $name"
+
+
+echo "Enter a number: "
+read no
+
+if (("$no" + 5 > 91 )); then
+        echo "$no is greater than 86"
+elif (( "$no" + 5 < 86 )); then
+        echo "$no is less than 86"
+else
+        echo "You entered 86"
+fi
