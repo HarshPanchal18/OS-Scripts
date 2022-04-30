ae4ac8a00310d456f9c932c9ad4869cb25945007
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-04-30 13:55:37 +0530

    Update whileLoop.sh

--- whileLoop.sh
+++ whileLoop.sh
@@ -7,3 +7,17 @@ do
         echo "i is now $i"
         i=$[$i+1]
 done
+
+
+i=0
+while [[ $i -lt 11 ]]
+do      # -lt = less than
+        if [[ "$i" == '5' ]] # space is mandatory
+        then 
+                echo "Loop Broken on Number $i"
+                break
+        fi
+        echo $i
+        ((i++))
+done
+echo "Done!"
