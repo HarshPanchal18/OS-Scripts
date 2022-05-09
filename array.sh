9556fd2326737b1d7a921f1faaed33dd4db67928
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-05-09 12:55:06 +0530

    Update array.sh

--- array.sh
+++ array.sh
@@ -34,3 +34,51 @@ echo ${arra[@]} # prints -> zero one two three four five
 arra=(minusOne "${arra[@]}")  # appends to the start
 
 echo ${arra[@]} # minusOne zero one two three four five
+
+
+
+i=3
+
+arra=("${arra[@]:0:$i}" onehalf "${arra[@]:$i}") # appends at 'i'th index
+
+echo ${arra[@]}
+
+echo ${!arra[@]} # prints index(s)
+
+unset -v 'arra[0]' # removes 0th index & value both
+
+echo ${arra[@]} # zero one onehalf two three four five
+
+echo ${!arra[@]} # 1 2 3 4 5 6 7
+
+array=("${arr[@]}" "${arra[@]}") # merging to the new one
+
+echo ${array[@]} # 1 2 3 4 5 6 7 8 9 10 zero one onehalf two three four five
+
+echo ${#array[@]} # returns array length (17)
+
+
+
+
+declare -A aa
+
+aa=([hello]=world [ab]=cd ["key space"]="hii linux")
+
+echo ${aa[@]} # cd world hii linux
+
+echo ${aa[ab]} # cd
+
+for key in "${!aa[@]}";
+do
+        echo "key: $key"
+        echo "value: ${aa[$key]}"
+done
+
+<<op
+key: ab
+value: cd
+key: hello
+value: world
+key: key space
+value: hii linux
+op
