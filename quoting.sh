bdc8ae42e0923aa28ef6d91977242f38f0e4eb6e
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-05-22 11:27:13 +0530

    Added results

--- quoting.sh
+++ quoting.sh
@@ -2,14 +2,18 @@
 
 calc='2 * 3'
 
-echo $calc
-echo "$calc"
-echo $(($calc))
+echo $calc 
+# prints 2, files of current directory, 3
+
+echo "$calc" # 2 * 3
+
+echo $(($calc)) # 6
 
 
 a='var'
-echo '$a' # prints '$a'
-echo "$a" # prints var
+echo '$a' # '$a'
+
+echo "$a" # var
 
 l1='
 '
@@ -21,11 +25,28 @@ l3=$'\n'
 empty=\
 
 echo "Line${l1} break"
-echo "Line${l2} break"
-echo "Line${l3} break"
-echo "No line break${empty} here"
+#Line
+# break
 
-echo $'Slash: [\\]'
-echo $'Tab again [\009]'
+echo "Line${l2} break"
+#Line
+# break
+
+echo "Line${l3} break"
+#Line
+# break
+
+echo "No line break${empty} here" #No line break here
+
+
+echo $'Slash: [\\]' # Slash: [\]
+
+echo $'Tab again [\009]' # Tab again [
+
 echo $'Form Feed: [\f]'
+#Form Feed: [
+              ]
+              
 echo $'Line\nBreak'
+#Line
+#Break
