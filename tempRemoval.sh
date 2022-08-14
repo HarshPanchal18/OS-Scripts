b0507edcf5a9fa8673cad11cbfbffc21c5303834
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-08-14 14:01:21 +0530

    Create tempRemoval.sh

---
+++ tempRemoval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Purpose: 
+# Created Date:  Saturday 21 May 2022 02:39:52 PM IST
+# Author: Harsh Panchal
+
+zenity --info --text="Files Deleted"
+rm *~
+rm .*~
+rm *.old
+rm *.bak
+rm *.OLD
+rm *.BAK
