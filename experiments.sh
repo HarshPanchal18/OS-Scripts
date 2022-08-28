50466fcf87b3e419005c0e217e05cd23cbcd5ce8
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-08-28 12:39:50 +0530

    Update experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -28,3 +28,43 @@ grep -V '^[[:space:]]*$'
 
 # Generate a "unique" 6-digit hexadecimal identifier.
 uuidgen -r | cut -c -6
+
+# Package: libmath-prime-util-perl
+# Prints all prime numbers between 60000 and 63000.
+primes 60000 63000 | column
+
+
+# Print /etc/passwd with nice columnar output
+
+column -t -s ':' /etc/passwd
+
+# OTHER SOLUTIONS:
+# tr ':' '\t' < /etc/passwd
+# awk -F: '{printf "%-18s %s %s %s %s %s %s\n", $1, $2, $3, $4, $5, $6, $7}' /etc/passwd
+
+
+#   Get the MAC addresses of the device from the ifconfig output.
+ifconfig | grep HW | awk 'BEGIN { print "Device     MAC address"
+print "------     ------" }
+{ printf "%-10s %s\n", $1, $5 }'
+
+#   Play a sound from terminal. Useful as and alert when
+#   a long task finishes.
+paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+
+
+### Making input invisible
+
+stty -echo     # makes input invisible
+stty echo      # to default
+
+# creating directory and files without name
+
+mkdir " "
+touch " "
+
+
+# creating directory -okgoogle
+mkdir -p -- "-okgoogle"
+#or
+mkdir -- "-okgoogle"
