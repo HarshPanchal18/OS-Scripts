91e7de3c46218d3128f4c21e4a1eae44c4c63d8a
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-09-08 10:38:30 +0530

    Create permissionsOfCurrDir.sh

---
+++ permissionsOfCurrDir.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#------------------------------------------
+# Purpose:
+# Created Date:  Tuesday 30 August 2022 06:48:51 PM IST
+# © Author: Harsh Panchal
+
+# Modify this script for your own purposes.
+
+# Usage:
+#------------------------------------------
+
+#find -type d \( \( -user root -perm /u=w \) -o \( -group root -perm /g=w \) -o -perm /o=w \)
+<<comm
+sudo find / -type d -user rinzwind -perm /u=w
+sudo find / -type d -group rinzwind -perm /g=w
+sudo find / -type d -perm /o=w
+comm
+
+echo "Executables: "
+for p;
+do
+    [ -x "$p" ] && echo "$p"
+done
+
+echo "Writables: "
+for p;
+do
+    [ -w "$p" ] && echo "$p"
+done
+
+echo "Readables: "
+for p;
+do
+    [ -r "$p" ] && echo "$p"
+done
