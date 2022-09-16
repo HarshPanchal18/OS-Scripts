1296251bcd8c18881836ff00ef9605c07edde689
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-09-16 19:35:11 +0530

    Create sourceme.sh

---
+++ sourceme.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+
+export a="hello world"
+alias sayHi="echo Hi"
+
+sayHello()
+{
+  echo Hello
+}
+
+<<c1
+after created,
+go to bash -> type 'source file.sh' OR '. file.sh'
+then type variables/Methods
+c1
