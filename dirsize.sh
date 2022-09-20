b75dbc74223eefd670590c79342e8f52b4c5c8ff
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-09-20 16:36:54 +0530

    Create dirsize.sh

---
+++ dirsize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#------------------------------------------
+# Purpose:
+# Created Date:  Monday 22 August 2022 11:41:39 AM IST
+# © Author: Harsh Panchal
+
+# Modify this script for your own purposes.
+
+# Usage:
+#------------------------------------------
+
+echo -n "Enter directory: "
+read -r x
+du -sh "$x"
