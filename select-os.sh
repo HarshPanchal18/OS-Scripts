cd292c12961e4a4eb4c6cd837f9e12ade00a6bee
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-10-06 13:27:29 +0530

    Create select-os.sh

---
+++ select-os.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Purpose: 
+# Created Date:  Sunday 22 May 2022 08:39:22 AM IST
+# Author: Harsh Panchal
+
+# Note 
+
+# START #
+
+select os in "linux" "windows" "mac"
+do
+    echo "${os}"
+    break
+done
