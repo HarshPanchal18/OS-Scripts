356c141fb6e526164561da55778d8e316ab530ae
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-11-20 15:52:50 +0530

    Create remove-trash.sh

---
+++ sys-performance/remove-trash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Purpose: 
+# Created Date:  Saturday 21 May 2022 02:39:52 PM IST
+# Author: Harsh Panchal
+
+# Note 
+
+# START #
+
+zenity --info --text="Files Deleted"
+rm *~
+rm .*~
+rm *.old
+rm *.bak
+rm *.OLD
+rm *.BAK
