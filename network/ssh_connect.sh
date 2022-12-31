deb372202f0c4dbf281e314807b37aa887fcc953
Harsh Panchal <63415584+HarshPanchal18@users.noreply.github.com>
2022-12-31 11:14:53 +0530

    Create ssh_connect.sh

---
+++ network/ssh_connect.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#------------------------------------------
+# Purpose:
+# Created Date:  Friday 22 July 2022 10:46:48 AM IST
+# Author: Harsh Panchal
+
+# Usage:
+#------------------------------------------
+
+server=(\
+    "username|password@server1.com" \
+    "username|password@server2.com" \
+    "username|password@server3.com" \
+    "username|password@server4.com" \
+    "username|password@server5.com" \
+    "username|password@server6.com" \
+    "username|password@server7.com" \
+)
+
+connection_type=( \
+    "ssh" "sftp" "ftp" "rsh" "telnet" "rlogin" "quit" \
+)
+
+clear
+printf "\nConnect to your predefined Server really fast.\n\n"
+PS3="Pick a server (or ctrl-c to quit): "
+
+select item in "${server[@]}"
+do
+    clear
+    echo "You are connecting to ${item}"
+    PS3="Choose connection type (or ctrl-c to quit): "
+
+    select type in "${connection_type[@]}"
+    do
+        read -p "Enter additional ${type} arguments \(or just press enter for none\): " args
+        case "{type}" in
+            ssh ) ssh "${args}" '${item}' ;;
+            rsh ) rsh "${args}" "${item}" ;;
+            ftp ) ftp "${args}" "${item}" ;;
+            rsh ) rsh "${args}" "${item}" ;;
+            telnet ) telnet "${args}" "${item}" ;;
+            rlogin ) rlogin "${args}" "${item}" ;;
+            quit ) break ;;
+            "" ) "please select one of the above or press ctrl-c" ;;
+        esac
+        exit 0
+    done
+    exit 0
+done
